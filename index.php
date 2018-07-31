e1e2a525d063ab04b06bbc26868a3d2c72556558
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:39:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 8</title>
+</head>
+<body>
+  <p>
+    <?php
+    $foot = 200;
+
+    while ($foot >= 0) {
+      echo ($foot -= 12) ." Enfin !!!! <br />";
+    }
+    ?>
+  </p>
+</body>
+</html>
